14d9de8bcac87b4da9e61d3be972fcf66392a191
Matthew Fitzpatrick <fnord@goatheckler.com>
2025-08-17 20:21:50 -0700

    add render-markdown.nvim

--- init.lua
+++ init.lua
@@ -1016,6 +1016,36 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  { -- Render rich Markdown in-buffer
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+       render_modes = { 'n', 'v' },
+       filetypes = { 'markdown' },
+       heading = { enabled = true, sign = true, border = true },
+       checkbox = { enabled = true, toggle_in_visual = true },
+       table = { enabled = true },
+       code = { enabled = true, sign = true, language_label = true },
+       latex = { enabled = false },
+       frontmatter = { enabled = true },
+       admonitions = {
+         enabled = true,
+         types = {
+           NOTE      = { icon = '', highlight = 'DiagnosticHint' },
+           TIP       = { icon = '', highlight = 'DiagnosticHint' },
+           IMPORTANT = { icon = '', highlight = 'DiagnosticInfo' },
+           WARNING   = { icon = '', highlight = 'DiagnosticWarn' },
+           CAUTION   = { icon = '', highlight = 'DiagnosticWarn' },
+           DANGER    = { icon = '', highlight = 'DiagnosticError' },
+         },
+         border = 'rounded',
+         padding = 1,
+         collapse_marker = true,
+       },
+     },
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
